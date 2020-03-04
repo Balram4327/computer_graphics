c8a2b4452706d20d8d1588efb9df42247e0a57b0
Balram <balram4327@gmail.com>
2020-03-04 14:27:28 +0530

    Ellipse

--- Circle/mp_circle.c
+++ Circle/mp_circle.c
@@ -1,23 +1,32 @@
 // C program for implementing 
 // Mid-Point Circle Drawing Algorithm 
 #include<stdio.h> 
+# include<graphics.h>
+
 
 // Implementing Mid-Point Circle Drawing Algorithm 
 void midPointCircleDraw(int x_centre, int y_centre, int r) 
 { 
 	int x = r, y = 0; 
+
+    int gd=DETECT, gm=VGAMAX;
+    initgraph(&gd, &gm, 0);
 	
 	// Printing the initial point on the axes 
 	// after translation 
-	printf("(%d, %d) ", x + x_centre, y + y_centre); 
+//	printf("(%d, %d) ", x + x_centre, y + y_centre);
+    putpixel(x_centre+x, y_centre + y,1); 
 	
 	// When radius is zero only a single 
 	// point will be printed 
 	if (r > 0) 
 	{ 
-		printf("(%d, %d) ", x + x_centre, -y + y_centre); 
-		printf("(%d, %d) ", y + x_centre, x + y_centre); 
-		printf("(%d, %d)\n", -y + x_centre, x + y_centre); 
+//		printf("(%d, %d) ", x + x_centre, -y + y_centre); 
+        putpixel(x_centre+x, y_centre - y,1); 
+//		printf("(%d, %d) ", y + x_centre, x + y_centre); 
+        putpixel(x_centre+y, y_centre + x,1); 
+//		printf("(%d, %d)\n", -y + x_centre, x + y_centre); 
+        putpixel(x_centre-y, y_centre + x,1);
 	} 
 	
 	// Initialising the value of P 
@@ -43,27 +52,38 @@ void midPointCircleDraw(int x_centre, int y_centre, int r)
 		
 		// Printing the generated point and its reflection 
 		// in the other octants after translation 
-		printf("(%d, %d) ", x + x_centre, y + y_centre); 
-		printf("(%d, %d) ", -x + x_centre, y + y_centre); 
-		printf("(%d, %d) ", x + x_centre, -y + y_centre); 
-		printf("(%d, %d)\n", -x + x_centre, -y + y_centre); 
+//		printf("(%d, %d) ", x + x_centre, y + y_centre); 
+        putpixel(x_centre+x, y_centre + y,1);
+//		printf("(%d, %d) ", -x + x_centre, y + y_centre); 
+        putpixel(x_centre-x, y_centre + y,1);
+//		printf("(%d, %d) ", x + x_centre, -y + y_centre); 
+        putpixel(x_centre+x, y_centre - y,1);
+//		printf("(%d, %d)\n", -x + x_centre, -y + y_centre); 
+        putpixel(x_centre-x, y_centre - y,1);
 		
 		// If the generated point is on the line x = y then 
 		// the perimeter points have already been printed 
 		if (x != y) 
 		{ 
-			printf("(%d, %d) ", y + x_centre, x + y_centre); 
-			printf("(%d, %d) ", -y + x_centre, x + y_centre); 
-			printf("(%d, %d) ", y + x_centre, -x + y_centre); 
-			printf("(%d, %d)\n", -y + x_centre, -x + y_centre); 
+//			printf("(%d, %d) ", y + x_centre, x + y_centre); 
+            putpixel(x_centre+y, y_centre + x,1);
+//			printf("(%d, %d) ", -y + x_centre, x + y_centre); 
+            putpixel(x_centre-y, y_centre + x,1);
+//			printf("(%d, %d) ", y + x_centre, -x + y_centre); 
+            putpixel(x_centre+y, y_centre -x,1);
+//			printf("(%d, %d)\n", -y + x_centre, -x + y_centre); 
+            putpixel(x_centre-y, y_centre -x,1);
 		} 
 	} 
+
+getchar();
+closegraph();
 } 
 
 // Driver code 
 int main() 
 { 
 	// To draw a circle of radius 3 centred at (0, 0) 
-	midPointCircleDraw(100, 100, 3); 
+	midPointCircleDraw(100, 100, 100); 
 	return 0; 
 } 
